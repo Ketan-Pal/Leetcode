c3c91f5f4054b40d75dcac116dad0fbebeacf3a2
Ketan Pal <88307951+Ketan-Pal@users.noreply.github.com>
2023-10-06 07:44:58 +0530

    Time: 6 ms (79.54%) | Memory: 44.3 MB (28.47%) - LeetSync

--- LeetCode/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ LeetCode/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,20 +1,14 @@
 class Solution {
     public int firstUniqChar(String s) {
         int j;
-        Integer[] count = Collections.nCopies(s.length(), 0).toArray(new Integer[0]);
+        int[] count = new int['z'+1];
 
         for(int i =0;i<s.length();i++){
-            for(j=i+1;j<s.length();j++){
-                if(s.charAt(i)==s.charAt(j)){
-                    count[i]++;
-                    count[j]++;
-                    break;
-                }
-            }
+            count[s.charAt(i)]++;
         }
 
-        for(int i = 0;i<count.length;i++){
-            if(count[i]==0) return i;
+        for(int i = 0;i<s.length();i++){
+            if(count[s.charAt(i)]==1) return i;
         }
         return -1;
         
